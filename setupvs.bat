f08d42180bcdec2cacaaf38ab8ba7d98bb78d38b
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2019-12-27 00:45:18 +0100

    Updated setupvs.bat

--- setupvs.bat
+++ setupvs.bat
@@ -1,7 +1,8 @@
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -products Microsoft.VisualStudio.Product.BuildTools -version [15.0,16.0) 
-wget "https://aka.ms/vs/15/release/vs_buildtools.exe" -O vs_buildtools.exe
-echo Exit Code is %errorlevel%
-vs_buildtools.exe update --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --passive --norestart --wait --includeRecommended
-echo Exit Code is %errorlevel%
-vs_buildtools.exe modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --passive --norestart --wait --includeRecommended --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.WinXP --add Microsoft.VisualStudio.Component.WinXP
-echo Exit Code is %errorlevel%
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -products Microsoft.VisualStudio.Product.BuildTools -version [15.0,16.0) >> out.log
+wget "https://aka.ms/vs/15/release/vs_buildtools.exe" -O vs_buildtools.exe >> out.log
+echo Exit Code is %errorlevel% >> out.log
+rem vs_buildtools.exe update --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --passive --norestart --wait --includeRecommended >> out.log
+rem echo Exit Code is %errorlevel% >> out.log
+vs_buildtools.exe modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --passive --norestart --wait --includeRecommended --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.WinXP --add Microsoft.VisualStudio.Component.WinXP >> out.log
+echo Exit Code is %errorlevel% >> out.log
+exit
